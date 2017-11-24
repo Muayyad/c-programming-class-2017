e700515323fe3e35a9f89ee974c2254f8b3fb56a
Muayyad <moy.diab@gmail.com>
2017-11-24 16:12:39 +0200

    Create loop-until-user-enter-char.c

---
+++ loop-until-user-enter-char.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main () {
+  char c;
+  do {
+    printf("press y to continue:\n");
+    scanf(" %c", c);
+    
+    //put your code here
+      
+  } while (c == 'y');
+
+  return 0;
+}
